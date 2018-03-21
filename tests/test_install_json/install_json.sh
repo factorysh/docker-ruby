a41a4ff6a1b16480350b5e86c91c574880ed069f
Mathieu Lecarme <mlecarme@bearstech.com>
2018-03-21 11:00:13 +0100

    Fix: Bundle exec

--- tests/test_install_json/install_json.sh
+++ tests/test_install_json/install_json.sh
@@ -3,4 +3,4 @@
 apt-get update
 apt-get install -y libgmp-dev
 bundle install --path=vendor
-ruby -e "require 'json'; puts ({hello: 'world'}).to_json"
+bundle exec ruby -e "require 'json'; puts ({hello: 'world'}).to_json"
