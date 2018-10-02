99ccd52465581a284e6cc49c2e5e3b487608f0ec
Mathieu Lecarme <mlecarme@bearstech.com>
2018-10-02 17:26:34 +0200

    Fix: tests wait for its db

--- tests_ruby/test_install_db/test_mysql.rb
+++ tests_ruby/test_install_db/test_mysql.rb
@@ -2,7 +2,17 @@
 
 require 'mysql2'
 
-client = Mysql2::Client.new host: 'maria', username: 'test', password: 'example', database: 'demo'
+client = nil
+for i in 1..30 do
+  begin
+    client = Mysql2::Client.new host: 'maria', username: 'test', password: 'example', database: 'demo'
+  rescue
+    puts 'oups'
+    sleep 1
+  else
+    break
+  end
+end
 
 results = client.query("SELECT 1+1 AS math")
 results.each do |row|
--- tests_ruby/test_install_db/test_pg.rb
+++ tests_ruby/test_install_db/test_pg.rb
@@ -2,8 +2,17 @@
 
 require 'pg'
 
-conn = PG.connect 'postgresql://test:example@pg/demo'
-
+conn = nil
+for i in 1..30 do
+  begin
+    conn = PG.connect 'postgresql://test:example@pg/demo'
+  rescue
+    puts 'oups'
+    sleep 1
+  else
+    break
+  end
+end
 conn.exec 'SELECT 1+1 AS math' do |result|
   result.each do |row|
     puts 'pg: %s' % row.values_at('math')
