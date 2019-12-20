c4d9ad837ec2cc7dc5749291b3236006396ae912
Mathieu Lecarme <mlecarme@bearstech.com>
2019-12-20 10:42:12 +0100

    Fix: shellcheck.

--- tests_ruby/test_install_db/install_db.sh
+++ tests_ruby/test_install_db/install_db.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 rm -rf vendor
-ln -sf $RUBY_VERSION/Gemfile Gemfile
-ln -sf $RUBY_VERSION/Gemfile.lock Gemfile.lock
+ln -sf "$RUBY_VERSION/Gemfile" Gemfile
+ln -sf "$RUBY_VERSION/Gemfile.lock" Gemfile.lock
 bundle install --path=vendor
