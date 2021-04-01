18cf1535d89d30caef91aafb6f20a5f7c991c81d
Mathieu Lecarme <mlecarme@bearstech.com>
2021-04-01 21:42:02 +0200

    More bullseye in tests.

--- tests_ruby/test_install_db/install_db.sh
+++ tests_ruby/test_install_db/install_db.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
 
+VERSION=$(ruby --version | cut -c 6-8)
+
 rm -rf vendor
 ln -sf "$RUBY_VERSION/Gemfile" Gemfile
 ln -sf "$RUBY_VERSION/Gemfile.lock" Gemfile.lock
-bundle install --path=vendor
+if [ "$VERSION" = "2.7" ]; then
+	bundle config set path vendor
+	bundle install
+else
+	bundle install --path=vendor
+fi
