2254c97d303dde7141c8fc474a18f5444b656cbb
Wilfried OLLIVIER <wollivier@bearstech.com>
2021-11-22 18:50:28 +0100

    Fix: warning is from 2.4 and later

--- tests_ruby/test_install_db/install_db.sh
+++ tests_ruby/test_install_db/install_db.sh
@@ -6,7 +6,7 @@ rm -rf vendor
 ln -sf "$RUBY_VERSION/Gemfile" Gemfile
 ln -sf "$RUBY_VERSION/Gemfile.lock" Gemfile.lock
 # check ruby version
-if [ "$(ruby -e "Gem::Version.new('$VERSION') >= Gem::Version.new('2.7') ? puts('0') : puts('1')")" -ne 0 ]
+if [ "$(ruby -e "Gem::Version.new('$VERSION') >= Gem::Version.new('2.4') ? puts('0') : puts('1')")" -ne 0 ]
 then
 	bundle install --path=vendor
 else
--- tests_ruby/test_install_json/install_json.sh
+++ tests_ruby/test_install_json/install_json.sh
@@ -4,7 +4,7 @@ apt-get update
 apt-get install -y libgmp-dev
 rm -rf vendor
 # check ruby version
-if [ "$(ruby -e "Gem::Version.new('$VERSION') >= Gem::Version.new('2.7') ? puts('0') : puts('1')")" -ne 0 ]
+if [ "$(ruby -e "Gem::Version.new('$VERSION') >= Gem::Version.new('2.4') ? puts('0') : puts('1')")" -ne 0 ]
 then
 	bundle install --path=vendor
 else
